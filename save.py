430429a6a5a45761274623d8675e3d74c9e5960e
Niterval <jr.vinhas@hotmail.com>
2022-05-26 00:41:17 -0300

    Desafio dia 7

--- save.py
+++ save.py
@@ -1,4 +1,5 @@
 import csv
+import gspread
 
 def save_to_csv(jobs):
   file = open('jobs.csv', 'w')
@@ -7,3 +8,12 @@ def save_to_csv(jobs):
 
   for job in jobs:
     write.writerow(list(job.values()))
+
+
+def google_sheet():
+  gc = gspread.service_account(filename='credentials.json')
+  sh = '1DZGu8GXH9zpJ_BLSJWCbE8RYdbpHuqi64CJoqoyb7Gw'
+  content = open('jobs.csv', 'r').read().encode('utf-8')
+  gc.import_csv(sh, content)
+
+  return
