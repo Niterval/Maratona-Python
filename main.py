6a1cee89d2068c871fcf03ab299a627366a4b11e
Niterval <jr.vinhas@hotmail.com>
2022-05-26 00:40:26 -0300

    Desafio dia 7

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from indeed import search_indeed
-from save import save_to_csv
+from save import save_to_csv, google_sheet
 
 search = 'python'
 
 result_indeed = search_indeed(search)
+save_drive_sheet = google_sheet()
 
 save_to_csv(result_indeed)
